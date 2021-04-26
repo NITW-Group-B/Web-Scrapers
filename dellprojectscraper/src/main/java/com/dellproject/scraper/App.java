c56621db1da35b1f776b53c4655fd1eafe127c33
David Tauraso <david318@me.com>
2021-04-26 15:11:09 -0700

    Got some web scraping done

--- dellprojectscraper/src/main/java/com/dellproject/scraper/App.java
+++ dellprojectscraper/src/main/java/com/dellproject/scraper/App.java
@@ -10,11 +10,13 @@ import org.jsoup.UnsupportedMimeTypeException;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.sputnikdev.bluetooth.URL;
 
-import net.bytepowered.common.lang.HashMap;
+// import net.bytepowered.common.lang.HashMap;
+// import okhttp3.benchmarks.UrlConnection;
 
 // import BeautifulSoup
-
+// I'm after steven J after about an hr
 // https://www.codegrepper.com/code-examples/python/how+to+scrape+.ashx
 public class App {
 
@@ -24,11 +26,43 @@ public class App {
       HashMap<String,String> variable = new HashMap<String, String>();
       variable.put("type", "application/x-google-chrome-pdf");
         // Here we create a document object and use JSoup to fetch the website
-      Document doc = Jsoup.connect("https://www.codetriage.com/?language=Java").get();
-      Document doc2 = Jsoup.connect("https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/problem12.html").get();
+      Document doc = Jsoup.connect("https://www.cruisemapper.com/wiki/759-how-much-does-a-cruise-ship-cost").get();
+      Elements repositories = doc.getElementsByClass("table table-striped");
+      Elements stuff = doc.select("th[style]");
+       //("height: 62px;");
+      // System.out.println(repositories);
+      System.out.println(stuff);
 
-        // don't know how to scrape this site
+      /*
+      header: 
+        th style= height: 62px;
 
+      body rows
+        td style= height: 36px;
+        6 items per row
+      our table data structure:
+
+      {
+        'header': [stuff],
+        'rows' : [stuff]
+      }
+
+      information importance:
+      ship name
+      cost to build
+      year built
+
+      */
+
+
+      // don't know how to scrape this page
+
+      // attempt 1
+      // Document doc2 = Jsoup.connect("https://cruising.org/-/media/research-updates/research/2021-state-of-the-cruise-industry_optimized.ashx").ignoreContentType(true).get();
+      // System.out.println(doc2);
+
+
+      // attempt 2
       // // Initialize UnSupportedMimeTypeExeception class 
       // UnsupportedMimeTypeException mimeType = new UnsupportedMimeTypeException("Hey this is Mime",
       //     "application/x-google-chrome-pdf",
@@ -41,7 +75,19 @@ public class App {
       // // .cookies(response.cookies())
       // .ignoreContentType(true)
       // .get();
-      System.out.println(doc2);
+      // System.out.println(test);
+
+
+      // attempt 3 (doesn't compile)
+      // URL url = 
+      // new URL( "https://github.com/NITW-Group-B/Web-Scrapers/blob/david-branch/2021%20State%20of%20the%20Cruise%20Industry_optimized.pdf" );
+
+      // UrlConnection connection = url.openConnection();
+
+      // input = connection.getInputStream();
+
+      // Document doc3 = Jsoup.parse(input, "UTF-8");
+      // System.out.println(doc3.toString());
 
       // With the document fetched, we use JSoup's title() method to fetch the title
       // System.out.printf("Title: %s\n", doc.title());
